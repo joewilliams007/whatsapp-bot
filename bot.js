06096c4a5e764167a8695418825aa2a8be3a1fe4
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-10 10:15:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,4 @@
 import makeWASocket, { DisconnectReason } from '@adiwajshing/baileys'
-import { Boom } from '@hapi/boom'
 
 async function connectToWhatsApp () {
     const sock = makeWASocket({
@@ -9,7 +8,7 @@ async function connectToWhatsApp () {
     sock.ev.on('connection.update', (update) => {
         const { connection, lastDisconnect } = update
         if(connection === 'close') {
-            const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+            const shouldReconnect = (lastDisconnect.error)?.output?.statusCode !== DisconnectReason.loggedOut
             console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
             // reconnect if not logged out
             if(shouldReconnect) {
@@ -23,7 +22,7 @@ async function connectToWhatsApp () {
         console.log(JSON.stringify(m, undefined, 2))
 
         console.log('replying to', m.messages[0].key.remoteJid)
-        await sock.sendMessage(m.messages[0].key.remoteJid!, { text: 'Hello there!' })
+        await sock.sendMessage(m.messages[0].key.remoteJid, { text: 'Hello there!' })
     })
 }
 // run in main file
