aa9c8bf97557afd9e3be3f0489bfc7f3f35a24cf
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-10 15:56:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// const qrcode = require('qrcode-terminal');
+const qrcode = require('qrcode-terminal');
 
 const { Client } = require('whatsapp-web.js');
 const client = new Client();
 
-// client.on('qr', qr => {
-//    qrcode.generate(qr, {small: true});
-// });
+client.on('qr', qr => {
+   qrcode.generate(qr, {small: true});
+});
 
-// client.on('ready', () => {
-//     console.log('Client is ready!');
-// });
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
 
 client.on('message', msg => {
     switch(msg.body) {
