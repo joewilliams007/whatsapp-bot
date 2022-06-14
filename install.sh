f96f88f649a825264fdb62266a8b0dfccc68a617
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-14 16:01:47 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ sudo chmod a+rx /usr/local/bin/yt-dlp
 npm i get-json
 npm i youtube-search-without-api-key
 npm install youtubei.js@latest
-npm install image-charts --save
-npm install anychart-nodejs
+npm i jsdom
+npm i d3
