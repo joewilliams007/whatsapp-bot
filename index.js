e0f8646cc640bd787dc9f796ef352a77c1975eaf
Universe <83557178+joewilliams007@users.noreply.github.com>
2022-06-14 18:07:04 +0200

    o

--- index.js
+++ index.js
@@ -80,12 +80,18 @@ var isCommand = false;
 
 var yourDate = new Date()
 var dd = yourDate.toISOString().split('T')[0]
-connection.query( // save message
-`INSERT INTO Messages (number, clearnumber, pushname, message, type, hasMedia, timestamp, deviceType, hasQuotedMsg, isGif, isForwarded, isCommand, date) 
-VALUES ("${number}","${number.split("@")[0]}","${msg._data.notifyName}","${msg.body}","${msg.type}","${msg.hasMedia}",${msg.timestamp},"${msg.deviceType}","${msg.hasQuotedMsg}","${msg.isGif}","${msg.isForwarded}","${isCommand}","${dd}")`
-, function (error, results, fields) {
-        if (error) console.log(error.message)
-});
+
+codeM()
+async function codeM(){
+    const codeM = await client.getCountryCode(number)
+
+        connection.query( // save message
+    `INSERT INTO Messages (number, clearnumber, pushname, message, type, hasMedia, timestamp, deviceType, hasQuotedMsg, isGif, isForwarded, isCommand, date, country_code) 
+    VALUES ("${number}","${number.split("@")[0]}","${msg._data.notifyName}","${msg.body}","${msg.type}","${msg.hasMedia}",${msg.timestamp},"${msg.deviceType}","${msg.hasQuotedMsg}","${msg.isGif}","${msg.isForwarded}","${isCommand}","${dd}","${codeM}")`
+    , function (error, results, fields) {
+            if (error) console.log(error.message)
+    });
+}
 
 
 // msg.reply("MSG.AUTHOR: "+msg.author+"\nMSG.FROM: "+msg.from+"\nCHOOSINGs: "+number)
